33c22df214fedfd51e36811fa7d838325786fe18
Vegar1 <40041927+Vegar1@users.noreply.github.com>
2021-02-04 14:53:38 +0100

    Update main.js

--- alias/main.js
+++ alias/main.js
@@ -1,11 +1,4 @@
-var ordliste = ["Kinetisk Energi", "Potensiell Energi", "Mekanisk Energi", "Energi", "Arbeid", "Virkningsgrad", "Effekt", "Joule", "Kraft", "Watt", "Vinkel", "Fart", "Akselerasjon", "Friksjonsarbeid"];
-for(let i = ordliste.length — 1; i > 0; i--){
-  const j = Math.floor(Math.random() * i)
-  const temp = ordliste[i]
-  ordliste[i] = ordliste[j]
-  ordliste[j] = temp
-}
-
 function randomWord() {
-  document.getElementById("alias").innerHTML = ordliste.shift();  
+  var ordliste = ["Kinetisk Energi", "Potensiell Energi", "Mekanisk Energi", "Energi", "Arbeid", "Virkningsgrad", "Effekt", "Joule", "Kraft", "Watt", "Vinkel", "Fart", "Akselerasjon", "Friksjonsarbeid"];
+  document.getElementById("alias").innerHTML = ordliste[Math.floor(Math.random() * ordliste.length)];
 }
